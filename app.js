2b43cbfd75c49d45e5b440336be3946732f17f1d
Atuld02 <atulbelievethat02@gmail.com>
2023-07-28 21:34:02 +0530

    DOM Learning

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-function c() {
-  setTimeout(() => {
-    printData();
-  }, 3000);
-}
-function printData() {
-  console.log(document.getElementById("numData").value);
-}
+// function c() {
+//   setTimeout(() => {
+//     printData();
+//   }, 3000);
+// }
+// function printData() {
+//   console.log(document.getElementById("numData").value);
+// }
 
 //c();
+const input = document.querySelector("input");
+
+const ipnum = document.getElementById("inputnum");
+function printdiv(e) {
+  //ipnum.textContent.toString(document.getElementById("numData").value);
+  //console.log(document.getElementById("numData").value);
+  //   ipnum += data;
+  //console.log(e.key);
+  ipnum.textContent += e.key;
+}
+
+input.addEventListener("keyup", printdiv);
