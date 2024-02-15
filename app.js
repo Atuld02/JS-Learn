a10bafad5f1dddf4c451046eb1768f1f443d6e30
Atul Dwivedi <atul.dwivedi@non.keysight.com>
2024-02-15 03:18:44 -0700

    showing 1 to N number

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
-// function c() {
-//   setTimeout(() => {
-//     printData();
-//   }, 3000);
-// }
-// function printData() {
-//   console.log(document.getElementById("numData").value);
-// }
 
-//c();
-const input = document.querySelector("input");
 
-const ipnum = document.getElementById("inputnum");
-function printdiv(e) {
-  //ipnum.textContent.toString(document.getElementById("numData").value);
-  //console.log(document.getElementById("numData").value);
-  //   ipnum += data;
-  //console.log(e.key);
-  ipnum.textContent += e.key;
+//console.log(x)
+
+function Print(){
+	let s=""
+	let x=document.querySelector("#num")
+	//console.log(x.value)
+	//console.log(x.textContent)
+	//if(x.value==0
+	var ans=Number(x.value)
+	//console.log(ans)
+	if(ans==0)
+		return
+	else{
+		for(let i=0;i<ans;i++){
+			if(i==0){
+				s+="<ul>"
+				s+="\n"
+			}
+			s+="<li>"
+			s+=(i+1)
+			s+="</li>"
+			s+="\n"
+		}
+		s+="</ul>"
+		s+="\n"
+	}
+	let curr=document.querySelector("#dis")
+	curr.textContent=s
 }
-
-input.addEventListener("keyup", printdiv);
