d6d34ae8cab84f7a1382867d1a74752eec13a00a
Atul Dwivedi <atul.dwivedi@non.keysight.com>
2024-02-16 04:58:15 -0700

    Cached cube

--- app.js
+++ app.js
@@ -5,29 +5,39 @@
 function Print(){
 	let s=""
 	let x=document.querySelector("#num")
-	//console.log(x.value)
-	//console.log(x.textContent)
-	//if(x.value==0
-	var ans=Number(x.value)
-	//console.log(ans)
-	if(ans==0)
-		return
-	else{
-		for(let i=1;i<=ans;i++){
-			//if(i==0){
-				s+="<li>"
-				s+="\n"
-			//}
-			s+="cube("
-			s+=(i)
-			s+=") = "
-			s+=((i)*(i)*(i))
-			s+="</li>"
+	if(localStorage.getItem(x.value))
+	{ var ans=Number(x.value)
+		if(ans==0)
+			return
+		else{
+			s+="<li>"
 			s+="\n"
+			s+="cube("
+			s+=(x.value)
+			s+=") = "
+			s+=((x.value)*(x.value)*(x.value))
+			s+=" (cached)</li>"
+			s+="\n"
+			}
 		}
-		//s+="<li>"
-		//s+="\n"
+	else{
+		var ans=Number(x.value)
+		if(ans==0)
+			return
+		else{
+			s+="<li>"
+			s+="\n"
+			s+="cube("
+			s+=(x.value)
+			s+=") = "
+			s+=((x.value)*(x.value)*(x.value))
+			s+=" (new)</li>"
+			s+="\n"
+			}
+		
+		localStorage.setItem(x.value,1)
 	}
+
 	let curr=document.querySelector("#dis")
 	curr.textContent=s
 }
