2d77438f944c046584b519c993b185082aa39d37
Atul Dwivedi <atul.dwivedi@non.keysight.com>
2024-02-19 08:21:11 -0700

    Debouncing

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-function printData() {
+
+ 
+
+const printData=() =>{
 	console.log(document.getElementById("num").value);
+}
+
+
+const debounce = function (fn, d) {
+	let timer;
+	return function () {
+	  clearTimeout(timer);
+	  timer = setTimeout(() => {
+		printData()
+	  }, d);
+	}
   }
+
+const do_Delay=debounce(printData,700)
