f594d12602b227b77e66764bfd27c4d3c96c14a6
Atul Dwivedi <atul.dwivedi@non.keysight.com>
2024-03-05 10:45:23 -0700

    cache cube

--- app.js
+++ app.js
@@ -1,67 +1,72 @@
-function Print(){
-	let s=""
-	let x=document.querySelector("#num")
-	var ans=Number(x.value)
-	let res=1
-    let timer=0
-	let a=""
-	//console.log(ans)
-	if(ans<=0){
-	s+="Cube of Less than or 0 not possible"
-	let curr=document.querySelector("#dis")
-	curr.textContent=s
-	return
+function Print() {
+  let s = "";
+  let x = document.querySelector("#num");
+  var ans = Number(x.value);
+  let res = 1;
+  let timer = 0;
+  let a = "";
+  //console.log(ans)
+  if (ans <= 0) {
+    s += "Cube of Less than or 0 not possible";
+    let curr = document.querySelector("#dis");
+    curr.textContent = s;
+    return;
+  }
+   A(ans);
+//   if (localStorage.getItem(ans)) {
+//       a=let outString = `cube(${ans}) = ${localStorage.getItem(ans)} (cached)`;
+    // console.log("true exists");
+//   } else {
+//     // calulcate_cached();
+// 	A(ans);
+//     timer = 1;
+//     // console.log("false not exists");
+//   }
+
+//   function calulcate_cached() {
+//     //let i=0
+//     for (let i = 0; i < ans; i++) {
+//       if (localStorage.getItem(i + 1)) {
+//         a = localStorage.getItem("1+" + (i + 1));
+//         console.log(i);
+//         console.log(a);
+//       } else {
+//         res = (i + 1) * (i + 1) * (i + 1);
+//         localStorage.setItem(i + 1, res);
+//         s += "cube(";
+//         s += i + 1;
+//         s += ") = ";
+//         s += localStorage.getItem(i + 1);
+//         s += " (new)";
+//         localStorage.setItem("1+" + (i + 1), s);
+//         a = localStorage.getItem("1+" + (i + 1));
+//         a += "(cached)";
+//         //res=1;
+//         s = "";
+//       }
+//       // console.log(typeof(a))
+//     }
+//   }
+
+function A(ans) {
+	let res = 0;
+	for (let i = 1; i <= ans; i++) {
+		let outString = `cube(${i}) = `;
+		let cachedVal = localStorage.getItem(i)
+		if (cachedVal) {
+			outString += `${cachedVal} (cached)`;
+		} else {
+			let cube = (i * i * i);
+			outString += `${cube} (new)`;
+			localStorage.setItem(i, cube);
+		}
+
+		//console.log(outString)
+		s+=outString
 	}
-     
-    if((localStorage.getItem(ans)))
-	 { timer=0
-		// console.log("true")
-	 }
-    else{ 
-		calulcate_cached()
-		timer=1
-		// console.log("false")
-	  }
-
-function calulcate_cached(){
-res=ans*ans*ans
-localStorage.setItem(ans,res)
-s+="<li>"
-s+="\n"
-s+="cube("
-s+=(x.value)
-s+=") = "
-s+=localStorage.getItem(ans)
-localStorage.setItem(("1+"+x.value),s)
-a=localStorage.getItem(("1+"+x.value))
-// console.log(typeof(a))
-}
-
-if(timer==1){
-	
-a+=" (new)</li>"
-}
-else{
-
-a=localStorage.getItem(("1+"+x.value))
-a+= "(cached)</li>"
-}
-
-a+="\n"
-let curr=document.querySelector("#dis")
-curr.textContent=a
-	
-
 }
 
 
-
-
-
-
-
-
-
-
-
-	
+  let curr = document.querySelector("#dis");
+  curr.textContent = s;
+}
