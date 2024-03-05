9f76659d0dcf84be99650b3d7b7bf360501cb3ee
Atul Dwivedi <atul.dwivedi@non.keysight.com>
2024-03-05 02:34:01 -0700

    Number Printing edited

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-
- 
-
-const printData=() =>{
-	console.log(document.getElementById("num").value);
-}
-
-
-const debounce = function (fn, d) {
-	let timer;
-	return function () {
-	  clearTimeout(timer);
-	  timer = setTimeout(() => {
-		printData()
-	  }, d);
+function Print(){
+	let s=""
+	let x=document.querySelector("#num")
+	var ans=Number(x.value)
+	//console.log(ans)
+	if(ans<=0){
+	s+="Entered number is less than 0"
+	let curr=document.querySelector("#dis")
+	curr.textContent=s
+	return 
 	}
-  }
-
-const do_Delay=debounce(printData,700)
+	s+="<ul>"
+	s+="\n"
+	for(let i=0;i<ans;i++){
+			s+="<li>"
+			s+=(i+1)
+			s+="</li>"
+			s+="\n"
+		}
+		s+="</ul>"
+		s+="\n"
+	let curr=document.querySelector("#dis")
+	curr.textContent=s
+}
